22a3550799924969622afefacb2432ba7f48c79f
JandyTenedora <jandy.tenedora@hotmail.com>
2020-09-30 17:50:14 +1000

    Adding batch queries

--- src/main/scala/batchAnalytics.scala
+++ src/main/scala/batchAnalytics.scala
@@ -1,10 +1,18 @@
+/*
+First project, to use spark functions to perform basic batch style queries on json logs
+
+To run the project:
+ mvn scala:run -DmainClass=batchAnalytics
+ */
+
+
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkContext
 import java.sql.Timestamp
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 
-//mvn scala:run -DmainClass=batchAnalytics
+
 
 case class WebLog(host: String,
                   timestamp: Timestamp,
@@ -23,7 +31,28 @@ object batchAnalytics {
     val preparedLogsNew = preparedLogs.withColumn("timestamp", col("timestamp").cast(TimestampType))
     val weblogs = preparedLogsNew.as[WebLog]
     println(weblogs.count)
-    println("\n\n\n\nRIGHT HEEERRRRREE!!!\n\n\n\n")
+    weblogs.show()
+
+    val topDailyUrls = weblogs.withColumn("dayOfMonth", dayofmonth($"timestamp"))
+      .select($"request", $"dayOfMonth")
+      .groupBy($"dayOfMonth", $"request")
+      .agg(count($"request").alias("count"))
+      .orderBy(desc("count"))
+
+    topDailyUrls.show()
+
+    val urlExtractor = """^GET (.+) HTTP/\d.\d""".r
+    val allowedExtensions = Set(".html",".htm", "")
+    val contentPageLogs = weblogs.filter {log =>
+      log.request match {
+        case urlExtractor(url) =>
+          val ext = url.takeRight(5).dropWhile(c => c != '.')
+          allowedExtensions.contains(ext)
+        case _ => false
+      }
+    }
+
+
   }
 }
 
